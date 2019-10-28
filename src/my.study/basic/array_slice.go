57693dc3707abb5f5873434ae4b3acce45e502ec
jinyang <jinyang>
2019-10-28 19:28:30 +0800

    切片打散传入可变参数

--- src/my.study/basic/array_slice.go
+++ src/my.study/basic/array_slice.go
@@ -74,7 +74,7 @@ func updateArraypointer(s *[10]int) {
 
 /*
 	这样声明的函数，调用的时候，入参只能是slice，不能是数组
-	在Go语言中，函数参数是按值传递的。当使用切片(slice)作为函数参数时，
+	函数参数是按值传递的。当使用切片(slice)作为函数参数时，
 	意味着函数将获得切片的副本：指向基础数组的起始地址的指针，以及切片的长度和容量
 */
 func updateSlice(s []int) {
@@ -82,6 +82,11 @@ func updateSlice(s []int) {
 }
 
 func rangeBak() {
+
+	//我们也可以只初始化某一个索引的值：
+	s := []int{4: 1}
+	fmt.Println(len(s), cap(s)) // 输出：5 5
+	fmt.Println(s)              // 输出：[0 0 0 0 1]
 
 	slice := []int{0, 1, 2, 3}
 	m := make(map[int]*int)
@@ -126,14 +131,27 @@ func slice2() {
 
 /*
 	可变参数函数
- */
+*/
 func multiPara(num ...int) {
 	num[0] = 18
 }
 
 func testmultiPara() {
 	i := []int{5, 6, 7}
+	/*
+		i... 表示将切片i打散,
+	*/
 	multiPara(i...)
+	
+	multiPara(1, 2)
+
+	/*
+		array := [2]{1,2}
+		只有切片可以用...打散, 数组不行
+		数组不能传入可变参数, 以下两种方式都是编译错误的
+		multiPara(array...)
+		multiPara(array)
+	*/
 	fmt.Println(i[0])
 
 	/*
@@ -148,7 +166,7 @@ func addValue(s *[]int) {
 		apppend函数返回的已经不是传入的slice的指针了， 是一个新指针，
 		为了能返回这个新指针， 就把入参定义为切片指针 *[]int， 即指向切片的指针
 		*s = 这样的赋值，是把指针的地址改了，即是一个新指针了
-	 */
+	*/
 	*s = append(*s, 3)
 	fmt.Printf("In addValue: s is %v\n", s)
 }
@@ -163,9 +181,8 @@ func addValuetest() {
 		In main, before addValue: s is [1 2]
 		In addValue: s is &[1 2 3]
 		In main, after addValue: s is [1 2 3]
-	 */
+	*/
 }
-
 
 func sliceExtract() {
 	a := [5]int{1, 2, 3, 4, 5}
@@ -201,17 +218,18 @@ func arrayLenth() {
 
 /*
 cap() 函数不适用 map。
- */
+*/
 
 func makeNon() {
 	s := make(map[string]int)
 	/*
-	删除 map 不存在的键值对时，不会报错，相当于没有任何作用；获取不存在的减值对时，
-	返回值类型对应的零值，所以返回 0。
-	 */
+		删除 map 不存在的键值对时，不会报错，相当于没有任何作用；获取不存在的减值对时，
+		返回值类型对应的零值，所以返回 0。
+	*/
 	delete(s, "h")
 	fmt.Println(s["h"])
 }
+
 /*
 nil 切片和空切片。nil 切片和 nil 相等，
 一般用来表示一个不存在的切片；
@@ -222,7 +240,7 @@ func nil1() {
 	var s2 = []int{}
 	if s2 == nil {
 		fmt.Println("yes nil")
-	}else{
+	} else {
 		fmt.Println("no nil")
 	}
 }
@@ -236,7 +254,7 @@ func nil1() {
 	容量的用途是：在数据拷贝和内存申请的消耗与内存占用之间提供一个权衡。
 	而长度的用途，则是为了帮助你限制切片可用成员的数量，提供边界查询的。
 	所以用 make 申请好空间后，需要注意不要越界【越 len 】
- */
+*/
 
 /*
 	数组或切片的截取操作。截取操作有带 2 个或者 3 个参数，形如：[i:j] 和 [i:j:k]，
@@ -254,26 +272,25 @@ func nil1() {
 	对于操作符 [i:j:k]，
 	k用来限制切片的容量，但是不能大于数组的长度 l，
 	截取得到的切片长度和容量计算方法是 j-i、k-i。
- */
+*/
 func sliceLenCap() {
 	s := [3]int{5, 6, 7}
 	a := s[:0]
-	fmt.Println("a=", a,  ", len(a)=", len(a), ", cap(a)=", cap(a))
-	b := s[:2]  // 从0开始索引, 取到的是第0个, 和第1个, 即5, 6
+	fmt.Println("a=", a, ", len(a)=", len(a), ", cap(a)=", cap(a))
+	b := s[:2] // 从0开始索引, 取到的是第0个, 和第1个, 即5, 6
 	fmt.Println("b=", b, ", len(b)=", len(b), ", cap(b)=", cap(b))
 	c := s[1:2:cap(s)]
 	fmt.Println("c=", c, ", len(c)=", len(c), ", cap(c)=", cap(c))
 	b[0] = 10
 	fmt.Println("s=", s)
-/*
-结果:
-   a= [] , len(a)= 0 , cap(a)= 3
-   b= [5 6] , len(b)= 2 , cap(b)= 3
-   c= [6] , len(c)= 1 , cap(c)= 2
-   s= [10 6 7]
- */
+	/*
+	   结果:
+	      a= [] , len(a)= 0 , cap(a)= 3
+	      b= [5 6] , len(b)= 2 , cap(b)= 3
+	      c= [6] , len(c)= 1 , cap(c)= 2
+	      s= [10 6 7]
+	*/
 }
-
 
 func ArrraySlice() {
 	fmt.Println("<-------------------------ArrraySlice begin -------------------> ")
