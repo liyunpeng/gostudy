06a299298dae0d8689573b2d02d782b59fc83bbd
lijinyang <kylin27@126.com>
2019-12-10 23:26:58 +0800

    结构体内部嵌入匿名结构体. 对象和指针的方法集试验

--- src/my.study/basic/struct.go
+++ src/my.study/basic/struct.go
@@ -4,44 +4,111 @@ import (
 	"bytes"
 	"fmt"
 	"reflect"
+	"unsafe"
 )
 
+type S struct {
+	m string
+}
+func (S) sVal(){}
+func (*S) sPtr(){}
+
+type Ta struct{
+	S
+}
+func (Ta) tVal(){
+	fmt.Println("aaaaaaaaaaaaa ")
+}
+func (*Ta) tPtr(){}
+
+func method1 (a interface{}){
+	t := reflect.TypeOf(a)
+	fmt.Println("t=", t, "t.nummethod=", t.NumMethod())
+
+	for i, n :=0,  t.NumMethod(); i< n; i++ {
+		m := t.Method(i)
+		fmt.Println("方法名=", m.Name, "方法类型=", m.Type)
+	}
+}
+
+func  test1()  {
+	var t Ta
+	t1 := reflect.TypeOf(t)
+	fmt.Println("t=", t1, "t.nummethod=", t1.NumMethod())
+
+	method1(t)
+
+	fmt.Println("---------------- method1 -------------------")
+	method1(&t)
+}
+/*
+	new一个结构体对象的标准写法
+*/
+func NewS() *S {
+	return &S{"foo"}
+}
+
 type dataStruct struct {
+	S     // 声明一个匿名结构体， S是上面定义的类型
 	num   int
 	key   *string
+	bool1 bool
 	items map[string]bool
+	in interface{}
 }
 
-/*
-	结构体里方法通过func后面加结构体变量声明完成
-	一般变量名命名为this
-	如果需要改变结构里的成员的值， 需要声明为指针
-*/
-func (this *dataStruct) pmethod() {
-	this.num = 7
+func (d *dataStruct) pointerMethod() {
+	d.num = 7
+	d.m = "pointerMethod from S "
 }
 
-
-
-func (this dataStruct) vmethod() {
-	this.num = 8
-	/*
-		声明的不是指针， 又想改变成员的值， 就在前面加*
-	*/
-	*this.key = "v.key"
-	this.items["vmethod"] = true
+func (d dataStruct) valueMethod() {
+	d.num = 8
+	//声明的不是指针， 又想改变成员的值， 就在前面加*
+	*d.key = "v.key"
+	d.items["valueMethod"] = true
 }
+
+func (d *dataStruct) pointerMethod1() {
+	fmt.Println("pointerMethod1 ")
+}
+
+func (d dataStruct) valueMethod1() {
+	fmt.Println("valueMethod1 ")
+}
+
 func struct1() {
+	var d0 dataStruct
+	fmt.Println("d0 长度=", unsafe.Sizeof(d0), "字节")
+	d0.pointerMethod1()
+	d0.valueMethod1()
+
+
+	var d1 *dataStruct
+	fmt.Println("d1 长度=", unsafe.Sizeof(d0), "字节")
+	d1.pointerMethod1()
+	d1.valueMethod1()
+
 	key := "key.1"
-	d := dataStruct{1, &key, make(map[string]bool)}
+	/*
+		结构体初始化时， 要写上字段名, 下面是结构体对象初始化标准姿势
+	*/
+	d := dataStruct{
+		num:   1,
+		key:   &key,
+		items: make(map[string]bool),
+	}
+	d.m = " from s"
+	fmt.Println("d=", d)
+
 	fmt.Printf("num=%v key=%v items=%v\n", d.num, *d.key, d.items)
 	//prints num=1 key=key.1 items=map[]
-	d.pmethod()
+	d.pointerMethod()
 	fmt.Printf("num=%v key=%v items=%v\n", d.num, *d.key, d.items)
 	//prints num=7 key=key.1 items=map[]
-	d.vmethod()
+	d.valueMethod()
 	fmt.Printf("num=%v key=%v items=%v\n", d.num, *d.key, d.items)
-	//prints num=7 key=v.key items=map[vmethod:true]
+	//prints num=7 key=v.key items=map[valueMethod:true]
 }
 
 func compare() {
@@ -92,28 +159,42 @@ func compare3() {
 	fmt.Println("bytes.Equal(b1, b2) b1 == b2:", bytes.Equal(b1, b2)) //prints: b1 == b2: true
 }
 
-type S struct {
-	m string
-}
-
-func f123() *S {
-	return &S{"foo"}  //A
-}
-
-func main() {
-	p := *(f123()) //B
-	fmt.Println(p.m) //print "foo"
-}
-
-
 func Struct() {
 	fmt.Println("<------------------ Struct begin ------------------> ")
+	test1()
 	struct1()
-
 	compare()
-
 	compare1()
 	compare3()
 
+	p := *(NewS())
+	fmt.Println(p.m)
 	fmt.Println("<--------------------- Struct end ----------------- >")
 }
+/*
+运行结果：
+<------------------ Struct begin ------------------>
+t= basic.Ta t.nummethod= 0
+t= basic.Ta t.nummethod= 0
+---------------- method1 -------------------
+t= *basic.Ta t.nummethod= 0
+d0 长度= 64 字节
+pointerMethod1
+valueMethod1
+d1 长度= 64 字节
+pointerMethod1
+panic: runtime error: invalid memory address or nil pointer dereference
+[signal 0xc0000005 code=0x0 addr=0x0 pc=0x7298f0]
+
+goroutine 1 [running]:
+my.study/basic.struct1()
+        F:/GoWorkSpace/gostudy/src/my.study/basic/struct.go:90 +0x2b0
+my.study/basic.Struct()
+        F:/GoWorkSpace/gostudy/src/my.study/basic/struct.go:165 +0x8a
+main.base()
+        F:/GoWorkSpace/gostudy/main.go:231 +0x6d
+main.main()
+        F:/GoWorkSpace/gostudy/main.go:72 +0x30a
+exit status 2
+
+ */
